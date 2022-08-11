785b7274ed032b52e994d9991dcbb79fb1bc8f51
Erik <svajgerramicerik@gmail.com>
2022-08-11 20:35:29 +0200

    update qr koda

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,3 +65,33 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// const findMyState = () => {
+//     const status = document.querySelector('.status');
+
+//     const success = (position) => {
+//         console.log(position)
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+//         // console.log(latitude + ' ' + longitude)
+
+//         const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+    
+//         fetch(geoApiUrl)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data)
+//             status.textContent = data.countryName
+//             // status.textContent = data.continentCode
+//         })
+    
+//     }
+
+//     const error = () => {
+//         status.textContent = 'Unable to retrive your location';
+//     }
+
+//     navigator.geolocation.getCurrentPosition(success, error);
+// }
+
+// document.querySelector('.find-state').addEventListener('click', findMyState);
